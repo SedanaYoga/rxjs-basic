bc32d707882661db9ce112da0344418cc48e78b2
Sedana Yoga <9g.yoga27@gmail.com>
2022-08-14 14:10:32 +0800

    Of Operator

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import { fromEvent } from 'rxjs'
+import { of } from 'rxjs'
 
-const observable = fromEvent(document, 'click')
+// `of` can be used if we want to loop in args level
+const observable = of(1, 2, 3, 4, 5)
 
-const subscription = observable.subscribe(console.log)
+const subscription = observable.subscribe({
+  next(value) {
+    console.log(value)
+  },
+  complete() {
+    // the operator complete after finishing the loop
+    console.log('completed')
+  },
+})
