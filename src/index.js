d52ddcc143b1c2419c08fd15fcaaf56e8dc94af8
Sedana Yoga <9g.yoga27@gmail.com>
2022-08-14 11:20:22 +0800

    Observers 3 main option object's method: next, complete, error

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import { Observable } from 'rxjs'
 
 const observable = new Observable((subscriber) => {
   subscriber.next('Hello World')
+  subscriber.error('Error!')
+  subscriber.next('test')
+
+  subscriber.complete()
+  subscriber.next('next')
 })
 
 observable.subscribe({
   next: (value) => {
     console.log(value)
+  },
+  complete: () => {
+    console.log('Complete called!')
+  },
+  error: (err) => {
+    console.error(err)
   },
 })
