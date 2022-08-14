e883849e1f636bc267523fb5c24e3f7f87ca6ba0
Sedana Yoga <9g.yoga27@gmail.com>
2022-08-14 14:13:54 +0800

    From Operator

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import { of } from 'rxjs'
+import { from } from 'rxjs'
 
-// `of` can be used if we want to loop in args level
-const observable = of(1, 2, 3, 4, 5)
+// `from` can be used if we want to loop in values inside an array/string/object
+const observable = from([1, 2, 3, 4, 5, 6])
+// const observable = from(fetch('https://jsonplaceholder.typicode.com/todos/1'))
 
 const subscription = observable.subscribe({
   next(value) {
