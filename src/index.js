d7643dbde29ce1810798d73ecdf881b26f5e862e
Sedana Yoga <9g.yoga27@gmail.com>
2022-08-14 11:56:59 +0800

    Timing Operator: interval

--- src/index.js
+++ src/index.js
@@ -1,28 +1,5 @@
-import { Observable } from 'rxjs'
+import { interval } from 'rxjs'
 
-const observable = new Observable((subscriber) => {
-  const id = setInterval(() => {
-    subscriber.next('test')
-    console.log('leak')
-  }, 1000)
+const observable = interval(1000)
 
-  return () => {
-    clearInterval(id) // clear the async function (setInterval)
-  }
-})
-
-const subscription = observable.subscribe({
-  next: (value) => {
-    console.log(value)
-  },
-  complete: () => {
-    console.log('Complete called!')
-  },
-  error: (err) => {
-    console.error(err)
-  },
-})
-
-setTimeout(() => {
-  subscription.unsubscribe()
-}, 4000)
+const subscription = observable.subscribe(console.log)
